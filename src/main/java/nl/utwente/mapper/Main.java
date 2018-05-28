1c48ab32cf638dcf08962c560b776db652494b3a
oplosthee <omniihd@gmail.com>
2018-05-28 15:26:08 +0200

    Add a Gradle task to run the project with samples

--- src/main/java/nl/utwente/mapper/Main.java
+++ src/main/java/nl/utwente/mapper/Main.java
@@ -37,27 +37,48 @@ import java.util.Set;
 
 public class Main {
 
-    /**
-     * Sample usage of the Adaptive Local Alignment algorithm.
-     */
     public static void main(String[] args) {
-        File source = new File(Main.class.getClassLoader().getResource("PATH_TO_SOURCE_JAR").getFile());
-        File target = new File(Main.class.getClassLoader().getResource("PATH_TO_TARGET_JAR").getFile());
+        File source = new File(Main.class.getClassLoader().getResource("samples/gamepack/123.jar").getFile());
+        File target = new File(Main.class.getClassLoader().getResource("samples/gamepack/170.jar").getFile());
 
         try {
             Map<String, ClassNode> sourceMap = JarLoader.readJar(source);
             Map<String, ClassNode> targetMap = JarLoader.readJar(target);
 
-            MethodNode sourceMethod = sourceMap.get("SOURCE_CLASS_NAME").methods.get(1);
-            MethodNode targetMethod = targetMap.get("TARGET_CLASS_NAME").methods.get(1);
+            MethodNode sourceMethod1 = sourceMap.get("ChatPlayer").methods.get(1);
+            MethodNode targetMethod1 = targetMap.get("defpackage/ChatPlayer").methods.get(2);
+
+            MethodNode sourceMethod2 = sourceMap.get("WorldMapManager").methods.get(1);
+            MethodNode targetMethod2 = targetMap.get("defpackage/WorldMapManager").methods.get(4);
+
+            // Manual inspection shows these methods actually look very different:
+            MethodNode sourceMethod3 = sourceMap.get("NPCComposition").methods.get(4);
+            MethodNode targetMethod3 = targetMap.get("defpackage/NPCComposition").methods.get(4);
+
+            MethodNode sourceMethod4 = sourceMap.get("Client").methods.get(11);
+            MethodNode targetMethod4 = targetMap.get("defpackage/Client").methods.get(3);
+
+            MethodNode unrelatedMethod1 = targetMap.get("defpackage/ChatPlayer").methods.get(3);
+            MethodNode unrelatedMethod2 = targetMap.get("defpackage/WorldMapManager").methods.get(5);
 
             Set<ClassNode> classes = new HashSet<>(sourceMap.values());
             classes.addAll(targetMap.values());
 
             AdaptiveLocalAlignment localAlignment = new AdaptiveLocalAlignment(-0.5f, new AdaptiveSubstitution(classes));
-            System.out.println("Similarity score: " + localAlignment.compare(sourceMethod, targetMethod));
+            System.out.println("[1] Score with same method of different revision: " + localAlignment.compare(sourceMethod1, targetMethod1));
+            System.out.println("[2] Score with same method of different revision: " + localAlignment.compare(sourceMethod2, targetMethod2));
+            System.out.println("[3] Score with same method of different (obfuscated) revision: " + localAlignment.compare(sourceMethod3, targetMethod3));
+            System.out.println("[4] Score with same method of different (obfuscated) revision: " + localAlignment.compare(sourceMethod4, targetMethod4));
+            System.out.println("-------------------------------------------------");
+            System.out.println("[5] Score with random method of different revision: " + localAlignment.compare(sourceMethod1, unrelatedMethod1));
+            System.out.println("[6] Score with random method of different revision: " + localAlignment.compare(sourceMethod2, unrelatedMethod2));
+            System.out.println("[7] Score with random method of different revision: " + localAlignment.compare(targetMethod1, unrelatedMethod1));
+            System.out.println("[8] Score with random method of different revision: " + localAlignment.compare(targetMethod2, sourceMethod1));
+            System.out.println("-------------------------------------------------");
+            System.out.println("[9] Comparing a method to itself: " + localAlignment.compare(sourceMethod1, sourceMethod1));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
